3ef450733c44813443768e9b29f90412c3dc3110
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-01-30 21:25:19 +0900

    Add Root

--- src/main/java/com/example/demo/controllers/DemoApplicationRestController.java
+++ src/main/java/com/example/demo/controllers/DemoApplicationRestController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplicationRestController {
 
+    @GetMapping("/")
+    public String root(){
+        return String.format("{Version:%s, Timestamp:%s}", "1.0", System.currentTimeMillis());
+    }
+    
     @GetMapping("/demo")
-    public String helloWorld(){
+    public String demo(){
         return String.format("{Timestamp:%s}", System.currentTimeMillis());
     }
 
